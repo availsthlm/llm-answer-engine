8d56264b011ad57f483d5e96389b353fe64bc9e7
Jonas Lindqvist <jonas@availsthlm.se>
2024-04-03 10:17:23 +0200

    Fixat sökresultatet

--- components/answer/SearchResultsComponent.tsx
+++ components/answer/SearchResultsComponent.tsx
@@ -51,9 +51,9 @@ const SearchResultsComponent = ({
             {Array.from({ length: isExpanded ? searchResults.length : 3 }).map(
                 (_, index) => (
                     <div key={index} className="p-2 w-full sm:w-1/2 md:w-1/4">
-                        <div className="flex items-center space-x-2 dark:bg-slate-700 bg-gray-100 p-3 rounded-lg h-full">
-                            <div className="w-5 h-5 dark:bg-slate-600 bg-gray-400 rounded animate-pulse"></div>
-                            <div className="w-full h-4 dark:bg-slate-600 bg-gray-400 rounded animate-pulse"></div>
+                        <div className="flex items-center space-x-2  bg-gray-100 p-3 rounded-lg h-full">
+                            <div className="w-5 h-5  bg-gray-400 rounded animate-pulse"></div>
+                            <div className="w-full h-4  bg-gray-400 rounded animate-pulse"></div>
                         </div>
                     </div>
                 )
@@ -63,12 +63,11 @@ const SearchResultsComponent = ({
 
     // 11. Render the 'SearchResultsComponent'
     return (
-        <div className="dark:bg-slate-800 bg-white shadow-lg rounded-lg p-4 mt-4">
+        <div className=" bg-white shadow-lg rounded-lg p-4 mt-4">
             <div className="flex items-center">
-                <h2 className="text-lg font-semibold flex-grow dark:text-white text-black">
+                <h2 className="text-lg font-semibold flex-grow  text-black">
                     Artiklar
                 </h2>
-                <img src="./brave.png" alt="brave logo" className="w-6 h-6" />
             </div>
             <div className="flex flex-wrap my-2">
                 {searchResults.length === 0 ? (
@@ -81,9 +80,9 @@ const SearchResultsComponent = ({
                             key={index}
                             className="p-2 w-full sm:w-1/2 md:w-1/4"
                         >
-                            <div className="flex items-center space-x-2 dark:bg-slate-700 bg-gray-100 p-3 rounded-lg h-full">
+                            <div className="flex items-center space-x-2  bg-gray-100 p-3 rounded-lg h-full">
                                 {!loadedFavicons[index] && (
-                                    <div className="w-5 h-5 dark:bg-slate-600 bg-gray-400 rounded animate-pulse"></div>
+                                    <div className="w-5 h-5  bg-gray-400 rounded animate-pulse"></div>
                                 )}
                                 <img
                                     src={result.favicon}
@@ -95,7 +94,7 @@ const SearchResultsComponent = ({
                                     href={result.link}
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="text-sm font-semibold truncate dark:text-gray-200 dark:hover:text-white text-gray-700 hover:text-black"
+                                    className="text-sm font-semibold truncate   text-gray-700 hover:text-black"
                                 >
                                     {result.title}
                                 </a>
@@ -107,7 +106,7 @@ const SearchResultsComponent = ({
                 <div className="w-full sm:w-full md:w-1/4 p-2">
                     <div
                         onClick={toggleExpansion}
-                        className="flex items-center space-x-2 dark:bg-slate-700 bg-gray-100 p-3 rounded-lg cursor-pointer h-12 justify-center"
+                        className="flex items-center space-x-2  bg-gray-100 p-3 rounded-lg cursor-pointer h-12 justify-center"
                     >
                         {!isExpanded ? (
                             <>
@@ -121,13 +120,13 @@ const SearchResultsComponent = ({
                                             className="w-4 h-4"
                                         />
                                     ))}
-                                <span className="text-sm font-semibold dark:text-gray-200 text-gray-700">
-                                    View more
+                                <span className="text-sm font-semibold  text-gray-700">
+                                    Visa mer
                                 </span>
                             </>
                         ) : (
-                            <span className="text-sm font-semibold dark:text-gray-200 text-gray-700">
-                                Show Less
+                            <span className="text-sm font-semibold  text-gray-700">
+                                Visa mindre
                             </span>
                         )}
                     </div>
