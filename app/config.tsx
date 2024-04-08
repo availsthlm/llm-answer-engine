674a6bc04ea2b182de89dc7a3504a9519414371f
Jonas Lindqvist <jonas@availsthlm.se>
2024-04-08 14:04:55 +0200

    useGroq hardcoded

--- app/config.tsx
+++ app/config.tsx
@@ -5,23 +5,19 @@
 // - IMPORTANT: Follow-up questions are not yet implrmented with Ollama models, only OpenAI compatible models that use  {type: "json_object"}
 
 const useGroq = process.env.USE_GROQ === "1";
-console.log("USE_GROQ", process.env.USE_GROQ);
+console.log("USE_GROQ", useGroq);
 export const config = {
     useOllamaInference: false,
     useOllamaEmbeddings: false,
-    inferenceModel: useGroq ? "mixtral-8x7b-32768" : "gpt-4-turbo-preview", // Groq: 'mixtral-8x7b-32768', 'gemma-7b-it' // OpenAI: 'gpt-3.5-turbo', 'gpt-4' // Ollama 'mistral', 'llama2' etc
-    inferenceAPIKey: useGroq
-        ? process.env.GROQ_API_KEY
-        : process.env.OPENAI_API_KEY, // Groq: process.env.GROQ_API_KEY // OpenAI: process.env.OPENAI_API_KEY // Ollama: 'ollama' is the default
+    inferenceModel: "mixtral-8x7b-32768", // Groq: 'mixtral-8x7b-32768', 'gemma-7b-it' // OpenAI: 'gpt-3.5-turbo', 'gpt-4' // Ollama 'mistral', 'llama2' etc
+    inferenceAPIKey: process.env.GROQ_API_KEY,
+    // Groq: process.env.GROQ_API_KEY // OpenAI: process.env.OPENAI_API_KEY // Ollama: 'ollama' is the default
     embeddingsModel: "text-embedding-3-small", // Ollama: 'llama2', 'nomic-embed-text' // OpenAI 'text-embedding-3-small', 'text-embedding-3-large'
     textChunkSize: 1000, // Recommended to decrease for Ollama
     textChunkOverlap: 400, // Recommended to decrease for Ollama
     numberOfSimilarityResults: 4, // Numbher of similarity results to return per page
     numberOfPagesToScan: 10, // Recommended to decrease for Ollama
-    nonOllamaBaseURL: useGroq
-        ? "https://api.groq.com/openai/v1"
-        : "https://api.openai.com/v1", //Groq: https://api.groq.com/openai/v1 // OpenAI:https://api.openai.com/v1
-
+    nonOllamaBaseURL: "https://api.groq.com/openai/v1", //Groq: https://api.groq.com/openai/v1 // OpenAI:https://api.openai.com/v1
     // Set LAN GPU server, example: http://192.168.1.100:11434/v1
     ollamaBaseUrl: process.env.OLLAMA_BASE_URL || "http://localhost:11434/v1",
     pineconeApiKey: process.env.PINECONE_API_KEY,
