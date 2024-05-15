cbe444b2b24d5737b69a5f903a4cf9b1a4507a3d
Jonas Lindqvist <jonas@availsthlm.se>
2024-05-15 10:42:35 +0200

    Changed to gpt-4o

--- app/config.tsx
+++ app/config.tsx
@@ -9,7 +9,7 @@ const useGroq = process.env.USE_GROQ === "1";
 export const config = {
     useOllamaInference: false,
     useOllamaEmbeddings: false,
-    inferenceModel: "gpt-4-turbo", // Groq: 'mixtral-8x7b-32768', 'gemma-7b-it' // OpenAI: 'gpt-3.5-turbo', 'gpt-4' // Ollama 'mistral', 'llama2' etc
+    inferenceModel: "gpt-4o", // Groq: 'mixtral-8x7b-32768', 'gemma-7b-it' // OpenAI: 'gpt-3.5-turbo', 'gpt-4' // Ollama 'mistral', 'llama2' etc
     inferenceAPIKey: process.env.OPENAI_API_KEY,
     // Groq: process.env.GROQ_API_KEY // OpenAI: process.env.OPENAI_API_KEY // Ollama: 'ollama' is the default
     embeddingsModel: "text-embedding-3-small", // Ollama: 'llama2', 'nomic-embed-text' // OpenAI 'text-embedding-3-small', 'text-embedding-3-large'
