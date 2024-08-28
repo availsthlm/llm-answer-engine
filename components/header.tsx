0799f101a829fd348e1b4707ceb6179df8d1a751
Jonas Lindqvist <jonas@availsthlm.se>
2024-08-28 22:02:50 +0200

    Uppdaterad logga

--- components/header.tsx
+++ components/header.tsx
@@ -1,18 +1,14 @@
 export function Header() {
-    return (
-        <header className="sticky z-50 flex justify-around w-full px-4 py-4 shrink-0  backdrop-blur-xl">
-            <a
-                href="https://availsthlm.se"
-                rel="noopener"
-                target="_blank"
-                className="Header__logo"
-            >
-                <img
-                    src={"/loggachefgpt.png"}
-                    alt="Chef GPT logo"
-                    width="320"
-                />
-            </a>
-        </header>
-    );
+  return (
+    <header className="sticky z-50 flex justify-around w-full px-4 py-4 shrink-0  backdrop-blur-xl">
+      <a
+        href="https://availsthlm.se"
+        rel="noopener"
+        target="_blank"
+        className="Header__logo"
+      >
+        <img src={"/logga.jpg"} alt="Chef GPT logo" width="320" />
+      </a>
+    </header>
+  );
 }
