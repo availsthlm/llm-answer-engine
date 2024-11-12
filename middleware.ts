9f6afb952fd2140d344c9496016d61f5ebb8a89c
Jonas Lindqvist <jonas@availsthlm.se>
2024-11-12 19:19:50 +0100

    Bypassing middleware 2

--- middleware.ts
+++ middleware.ts
@@ -6,31 +6,6 @@ export function middleware(request: NextRequest) {
   console.log("referer", referer);
   const response = NextResponse.next();
   return response;
-  // Set Content-Security-Policy header
-  response.headers.set(
-    "Content-Security-Policy",
-    `frame-ancestors 'self' http://localhost:3000 http://127.0.0.1:5500`
-  );
-
-  // Set X-Frame-Options for older browsers
-  response.headers.set("X-Frame-Options", "SAMEORIGIN");
-
-  // Define allowed hostnames
-  const allowedHostnames = ["localhost", "127.0.0.1"]; // Add more domains as needed
-
-  // Check if the request is coming from an allowed domain
-  if (referer) {
-    try {
-      const refererUrl = new URL(referer);
-      if (!allowedHostnames.includes(refererUrl.hostname)) {
-        return new NextResponse(null, { status: 403 });
-      }
-    } catch (error) {
-      return new NextResponse(null, { status: 403 });
-    }
-  }
-
-  return response;
 }
 
 // Make the matcher more specific to your needs
